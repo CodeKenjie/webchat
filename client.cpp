4a061c91d617cb97b6e4e22f96743b6010edab68
Kenjie <franciskenjiexd@gmail.com>
2025-07-30 20:28:20 +0800

    working -server and client

--- client.cpp
+++ client.cpp
@@ -1,7 +1,83 @@
+/* Client side Programming 
+    This is For Educational purposes only 
+            NETWORK PROGRAMMING (TCP)
+    -to learn or understand how the networking program function
+
+
+*/
 #include <iostream>
+// tipical networking libraries
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
 
 int main() {
+    int sock;
+    int opt = 1;
+    char buffer[BUFFER_SIZE] = {0}; // all elements zero -initalized
 
 
+    //create a socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    //check if the creation of the socket is successful
+    if (sock < 0){
+        std::cerr << "Socket Creation failed!" << std::endl;
+        exit(1);
+    }
+    // set a socket option
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+
+    // Set the value of the port and the port address
+    sockaddr_in saddr;
+    int addrlen = sizeof(saddr);
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(PORT);
+    // check if the inet_pton is working
+    if (inet_pton(AF_INET, "127.0.0.1", &saddr.sin_addr) < 0) {
+        std::cerr << "Invalid address / Address not supported" << std::endl;
+        exit(1);
+    }
+
+    // stablish a connection to a listening sever
+    if (connect(sock, (struct sockaddr*)&saddr, sizeof(saddr)) < 0){
+        std::cerr << "Connection Failed" << std::endl;
+        close(sock);
+        exit(1);
+    }
+
+    //std::cout << "Client connected to a server" << std::endl;
+
+    //send messege
+
+    const char* hello = "Hello from client";
+    ssize_t bytes_sent = send(sock, hello, strlen(hello), 0);
+
+    if (bytes_sent < 0) {
+        std::cerr << "Failed to send messege" << std::endl;
+        close(sock);   
+        exit(1);
+    }
+
+    std::cout << "Message sent: " << std::endl;
+
+    // recieve response
+    ssize_t smsg = recv(sock, buffer, BUFFER_SIZE, 0);
+
+    if (smsg < 0) {
+        std::cerr << "Recieve Failed" << std::endl;
+        close(sock);
+        exit(1);
+    }
     
+    buffer[smsg] = '\0';
+    std::cout << "Recieved: " << buffer << std::endl;
+
+
+    close(sock);
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -23,9 +23,6 @@
 int main() {
     int sock, n_sock;
     int opt = 1;
-    // declaring the sockaddr_in for making a port ---its basically a port that you can set;
-    sockaddr_in saddr;
-    int addrlen = sizeof(saddr);
     char buffer[BUFFER_SIZE] = {0};
 
     // this is the socket i have created
@@ -33,50 +30,59 @@ int main() {
 
     if (sock == 0) {
         perror("Socket Failed");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
     // set socket options (optional) (socket option reuse addr or socket addr reuse port)
     setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
-    // binding socket to a port
-
     //inside a sockaddr_in setting port value
+    // declaring the sockaddr_in for making a port ---its basically a port that you can set;
+    sockaddr_in saddr;
+    int addrlen = sizeof(saddr);
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(PORT);
     inet_pton(AF_INET, "127.0.0.1", &saddr.sin_addr); // setting a string sin_addr to a binary, for security purposes;;
     //sin_zero --only here for compatibility;
-
+    
+    
+    // binding socket to a port
     //the struct sockaddr contains sa_family
-
     // this statement binds the everything
     if (bind(sock, (struct sockaddr*)&saddr, sizeof(saddr)) < 0) {
         perror("Failed to Bind");
-        exit(EXIT_FAILURE);
+        close(sock);
+        exit(1);
     }
 
     //listen for connections
     //assign socket and listen, setting listen queue to 3 (can be greater than 3) this is just a maximum pending queue, and if it reaches 0 then fail the listen and exit
-    if (listen(sock, 3) < 0) {
+    if (listen(sock, 1) < 0) {
         perror("Listened");
-        exit(EXIT_FAILURE);
+        close(sock);
+        exit(1);
     }
 
     std::cout << "Server listening on port: " << PORT << std::endl;
 
-    // when a client connect accept it and it a socket
+    // when a client connect accept it and it give it a new socket
     //accept incoming connection
-    n_sock = accept(sock, (struct sockaddr*)&saddr, (socklen_t*)&addrlen);
+    sockaddr_in caddr;
+    socklen_t client_len = sizeof(caddr);
+
+    n_sock = accept(sock, (struct sockaddr*)&caddr, &client_len);
 
     if (n_sock < 0) {
         perror("Accept");
-        exit(EXIT_FAILURE);
+        close(n_sock);
+        exit(1);
     }
+
+    std::cout << "A client successfully connected!" << std::endl;
 
     // read data from client
     read(n_sock, buffer, BUFFER_SIZE);
     std::cout << "Messege from the client: " << buffer << std::endl;
-
     //send response
     const char* response = "Connected to the server;";
     send (n_sock, response, strlen(response), 0);
